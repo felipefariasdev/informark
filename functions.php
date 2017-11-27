0e70b4a5737267f7fa96cd8689a3857fd95d5aa3
Felipe Farias <190190f@gmail.com>
2017-11-27 01:40:42 -0200

    update functions

--- functions.php
+++ functions.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * @return mysqli
+ */
 function connection(){
 	$host = "localhost";        // alterar o host ex: site.com.br ou algum ip
 	$usuario = "root";          // alterar o usuário do banco de dados
@@ -9,6 +12,10 @@ function connection(){
 	if (mysqli_connect_errno()) trigger_error(mysqli_connect_error());
 	return $mysqli;
 }
+
+/**
+ * @param $mysqli
+ */
 function create_table($mysqli){
     $sql = "DROP TABLE `rev_temp`";
 	$mysqli->query($sql);
@@ -34,7 +41,47 @@ function create_table($mysqli){
 		}
 	}
 }
-function insert($arrayProcessos,$mysqli){
+/**
+ * @param $conteudoLine
+ * @return array
+ */
+function arrayLineSpace($conteudoLine){ // separa o conteudo de cada linha por espaço
+    $array_caracteres_spaces = explode(" ",$conteudoLine);
+    $array_caracteres_space_line = [];
+    foreach($array_caracteres_spaces as $array_caracteres_space){
+        $array_caracteres_space_line[] = $array_caracteres_space;
+    }
+    return $array_caracteres_space_line;
+}
+function getProcessos($arrayLineSpaces){ // armazena no array o conjunto de caracteres que foi identificado como um processo
+    /** @var TYPE_NAME $arrayProcessos */
+    $arrayProcessos = [];
+    foreach($arrayLineSpaces as $arrayLineSpace){                         // lendo os campos separado por espacos
+        $validatorProcesso = validatorProcesso($arrayLineSpace);             // verifica se esse campo é um processo
+        if($validatorProcesso!=false){
+            $arrayProcessos[] = $validatorProcesso;                 // adiciona os processos no array.
+        }
+    }
+    return $arrayProcessos;
+}
+function validatorProcesso($campo){ // verifica se aquele conjunto de caracteres é um processo
+    $campoClear         = trim($campo);                         // limpando os espaços
+    $qtdCaracteres      = strlen($campoClear);                  // quantidade de caracteres
+    if($qtdCaracteres==9) {                                     // verificando se tem 9 digitos
+        if( is_numeric($campoClear)){                           // verificando se é numero
+            return $campoClear;
+        }else{
+            return false;
+        }
+    }else{
+        return false;
+    }
+}
+/**
+ * @param $arrayProcessos
+ * @param $mysqli
+ */
+function insert($arrayProcessos, $mysqli){ // insert os processos
     foreach($arrayProcessos as $arrayProcesso){
         $sql = "INSERT INTO rev_temp (processo) VALUES ('".$arrayProcesso."')";
         $mysqli->query($sql);
@@ -48,39 +95,6 @@ function insert($arrayProcessos,$mysqli){
         }
         echo $sql;
         echo "\n";
-    }
-
-}
-function arrayLineSpace($conteudoLine){
-    $array_caracteres_spaces = explode(" ",$conteudoLine);
-    $array_caracteres_space_line = [];
-    foreach($array_caracteres_spaces as $array_caracteres_space){
-        $array_caracteres_space_line[] = $array_caracteres_space;
-    }
-    return $array_caracteres_space_line;
-}
-function getProcessos($arrayLineSpaces){
-    /** @var TYPE_NAME $arrayProcessos */
-    $arrayProcessos = [];
-    foreach($arrayLineSpaces as $arrayLineSpace){                         // lendo os campos separado por espacos
-        $validatorProcesso = validatorProcesso($arrayLineSpace);             // verifica se esse campo é um processo
-        if($validatorProcesso!=false){
-            $arrayProcessos[] = $validatorProcesso;                 // adiciona os processos no array.
-        }
-    }
-    return $arrayProcessos;
-}
-function validatorProcesso($campo){ /* verifica se o campo é realmente um processo */
-    $campoClear         = trim($campo);                         // limpando os espaços
-    $qtdCaracteres      = strlen($campoClear);                  // quantidade de caracteres
-    if($qtdCaracteres==9) {                                     // verificando se tem 9 digitos
-        if( is_numeric($campoClear)){                           // verificando se é numero
-            return $campoClear;
-        }else{
-            return false;
-        }
-    }else{
-        return false;
     }
 }
 ?>
